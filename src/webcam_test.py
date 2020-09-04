12b7dd9b03e65fc6df2cc69760138423ae3c423a
paulozk <p.m.ozkohen@student.rug.nl>
2020-09-04 16:01:47 +0100

    Webcam code works

---
+++ src/webcam_test.py
@@ -0,0 +1,54 @@
+import cv2
+from src.processing import *
+from src.model import *
+import sys
+import time
+
+# init CNN
+model = CNN(height=48, width=48, n_classes=7, learning_rate=0.001)
+model.load_model('weights\weights.h5')
+# init webcam
+cap = cv2.VideoCapture(0)
+# init face extractor
+extractor = FaceExtractor()
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+bottomLeftCornerOfText = (30, 200)
+fontScale = 1
+fontColor = (255, 255, 255)
+lineType = 2
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # detect face in frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    try:
+        face = extractor.detect(gray)[0]
+        # draw rectangle
+        cv2.rectangle(frame, (face[0], face[1]), (face[0] + face[2], face[1] + face[3]), (255, 0, 0), 2)
+        emotion = model.predict(gray)
+
+
+        cv2.putText(frame, model.show_output(emotion),
+                    bottomLeftCornerOfText,
+                    font,
+                    fontScale,
+                    fontColor,
+                    lineType)
+
+        time.sleep(0.1)
+
+
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    except:
+        pass
+    finally:
+        cv2.imshow('frame', frame)
+
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
